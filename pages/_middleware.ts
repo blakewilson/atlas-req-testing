029eaf127d74c5fcb7a1fc3ebdf4e4a8a1b74231
Blake Wilson <blake@blake.id>
2022-06-09 08:28:04 -0500

    Print more helpful info on middleware req

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,6 +2,9 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   console.log("Middleware request", req);
+  console.log("middleware request url", req.url);
+  console.log("middleware next url", req.nextUrl);
+  console.log("middleware headers", req.headers);
 
   NextResponse.next();
 }
